e2fbd9b90927ec57cb091ab582cc8c77d71bcca2
Alisson Lopes <alyssonboxx33@gmail.com>
2022-07-04 18:03:35 -0300

    Autenticação de Usuário

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,7 @@ namespace Biblioteca.Controllers
         public IActionResult Index()
         {
             Autenticacao.CheckLogin(this);
+            ViewData["mensagem"] = "Seja bem vindo ao sistema de controle de empréstimos de livros";
             return View();
         }
 
@@ -33,15 +34,16 @@ namespace Biblioteca.Controllers
         [HttpPost]
         public IActionResult Login(string login, string senha)
         {
-            if(login != "admin" || senha != "123")
-            {
+
+            if(Autenticacao.verificarLoginSenha(login,senha,this)){
+
+                return RedirectToAction("Index");
+
+            } else {
+
                 ViewData["Erro"] = "Senha inválida";
                 return View();
-            }
-            else
-            {
-                HttpContext.Session.SetString("user", "admin");
-                return RedirectToAction("Index");
+
             }
         }
 
